42a297f93fbc26691a056751aa811ef7a4a6ca95
sxl2958 <sxl2958@d25t50z2gq17im.amer.homedepot.com>
2019-03-14 10:34:54 -0400

    Add materialize to boiler plate app

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+// import { Navbar, NavItem } from "react-materialize";
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello From Home Component</h1>
+      </div>
+    );
+  }
+}
