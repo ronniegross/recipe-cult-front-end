988b3f517d0a914ac3193d31dcbb9d63e2a9949e
Spencer Lewis <spencerlewis@Spencers-MBP.attlocal.net>
2019-03-15 09:47:38 -0400

    refactor inputDisplay into pure component

--- src/components/InputDisplay.js
+++ src/components/InputDisplay.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 
-export default class InputDisplay extends Component {
-  render() {
-    return (
-      <div className="formInput">
-        {this.props.input1 ? <h6>Input 1 value: {this.props.input1}</h6> : null}
-        {this.props.input2 ? <h6>Input 2 value: {this.props.input2}</h6> : null}
-        {this.props.input3 ? <h6>Input 3 value: {this.props.input3}</h6> : null}
-      </div>
-    );
-  }
+export default function InputDisplay(props) {
+  return (
+    <div className="formInput">
+      {props.input1 ? <h6>Input 1 value: {props.input1}</h6> : null}
+      {props.input2 ? <h6>Input 2 value: {props.input2}</h6> : null}
+      {props.input3 ? <h6>Input 3 value: {props.input3}</h6> : null}
+    </div>
+  );
 }
