6d12afa546af01effbc6dcdecbd23c4b8b18aea0
Spencer Lewis <spencerlewis@Spencers-MBP.attlocal.net>
2019-03-15 10:13:40 -0400

    Refactor Navbar into Pure component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
-class Navbar extends Component {
-    state = {}
-    render() {
-        return (
-            <div>
-                <AppBar className="navbar" position="static" color="primary">
-                    <Toolbar>
-                        <Link to="/">
-                            <Button>Home</Button>
-                        </Link>
-                        <Link to="/form">
-                            <Button>Form</Button>
-                        </Link>
-                        <Link to="/table">
-                            <Button>Table</Button>
-                        </Link>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
-}
-
-export default Navbar;
+export default function Navbar() {
+    return (
+        <div>
+            <AppBar className="navbar" position="static" color="primary">
+                <Toolbar>
+                    <Link to="/">
+                        <Button>Home</Button>
+                    </Link>
+                    <Link to="/form">
+                        <Button>Form</Button>
+                    </Link>
+                    <Link to="/table">
+                        <Button>Table</Button>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
