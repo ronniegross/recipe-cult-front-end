5a532a3c696af69882d981b46f0224f08a2bce46
Spencer Lewis <spencerlewis@Spencers-MBP.attlocal.net>
2019-03-15 09:46:28 -0400

    refactor input form into pure component

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField"
 
-export default class InputForm extends Component {
-  render() {
+export default function InputForm(props) {
     return (
       <div>
         <div className="formInput">
         <TextField
           id="standard-name"
           label="Form Input 1"
-          value={this.props.input1}
-          onChange={this.props.handleInput1Change}
+          value={props.input1}
+          onChange={props.handleInput1Change}
           margin="normal"
         />
         </div>
@@ -19,8 +18,8 @@ export default class InputForm extends Component {
         <TextField
           id="standard-name"
           label="Form Input 2"
-          value={this.props.input2}
-          onChange={this.props.handleInput2Change}
+          value={props.input2}
+          onChange={props.handleInput2Change}
           margin="normal"
         />
         </div> 
@@ -28,8 +27,8 @@ export default class InputForm extends Component {
         <TextField
           id="standard-name"
           label="Form Input 3"
-          value={this.props.input3}
-          onChange={this.props.handleInput3Change}
+          value={props.input3}
+          onChange={props.handleInput3Change}
           margin="normal"
         />
         </div> 
@@ -37,11 +36,10 @@ export default class InputForm extends Component {
         <Button 
           variant="contained" 
           color="primary"
-          onClick={this.props.handleFormSubmit}>
+          onClick={props.handleFormSubmit}>
           Submit Form
         </Button> 
         </div>
       </div>
     );
-  }
 }
